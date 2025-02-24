91b7f1f9c6982b7b7cc3fd11ef104e4e353ca8f3
Evan Earle <evan.earle@gmail.com>
2025-02-24 19:35:49 -0400

    class feb 24

--- src/components/books/BookEdit.jsx
+++ src/components/books/BookEdit.jsx
@@ -1,16 +1,13 @@
-import { useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useParams, useNavigate } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { useForm } from "react-hook-form";
+import BookForm from "./BookForm";
 
 const BookEdit = () => {
   const { id } = useParams();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setValue,
-  } = useForm();
+
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { isPending, error, data } = useQuery({
     queryKey: ["books", id],
@@ -20,75 +17,41 @@ const BookEdit = () => {
     },
   });
 
-  useEffect(() => {
-    console.log("data", data);
-    setValue("title", data?.title);
-    setValue("author", data?.author);
-    setValue("published_year", data?.published_year);
-    setValue("genre", data?.genre);
-  }, [data]);
+  const editBookMutation = useMutation({
+    mutationFn: async (data) => {
+      const response = await fetch(`http://localhost:3000/books/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      return response.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["booksData"]);
+      navigate("/admin/books");
+    },
+  });
+
+  // useEffect(() => {
+  //   //pre-populate the form if data is available
+  //   setValue("title", data?.title);
+  //   setValue("author", data?.author);
+  //   setValue("published_year", data?.published_year);
+  //   setValue("genre", data?.genre);
+  // }, [data]);
+
+  const processData = (data) => {
+    editBookMutation.mutate(data);
+  };
 
   return (
     <div className="max-w-lg mx-auto bg-white shadow-md rounded-lg p-6">
       <h2 className="text-2xl font-bold mb-4 text-gray-800">
         Edit Book - Id: {data?.id}
       </h2>
-      <form className="space-y-4">
-        <div>
-          <input
-            {...register("title", { required: "Title is required!" })}
-            type="text"
-            placeholder="Title"
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          {errors.title && (
-            <p className="text-red-500 text-sm mt-1">{errors.title.message}</p>
-          )}
-        </div>
-        <div>
-          <input
-            {...register("author", { required: "Author is required!" })}
-            type="text"
-            placeholder="Author"
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          {errors.author && (
-            <p className="text-red-500 text-sm mt-1">{errors.author.message}</p>
-          )}
-        </div>
-        <div>
-          <input
-            {...register("published_year", { required: "Year is required!" })}
-            type="number"
-            min="1700"
-            max="2025"
-            placeholder="Year"
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          {errors.published_year && (
-            <p className="text-red-500 text-sm mt-1">
-              {errors.published_year.message}
-            </p>
-          )}
-        </div>
-        <div>
-          <input
-            {...register("genre", { required: "Genre is required!" })}
-            type="text"
-            placeholder="Genre"
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          {errors.genre && (
-            <p className="text-red-500 text-sm mt-1">{errors.genre.message}</p>
-          )}
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-all"
-        >
-          Create Book
-        </button>
-      </form>
+      <BookForm onDataCollected={processData} initialData={data} />
     </div>
   );
 };
